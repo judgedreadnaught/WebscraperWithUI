84e756021c409a131ae1783427846f9680363a59
judgedreadnaught <akhanal977@gmail.com>
2020-07-19 23:31:46 -0400

    Added more preventative measures to catch lingering errors and added numerous comments. This is most likely the finished alpha version of the application

--- WebscraperWithUI/src/application/ImageApp.java
+++ WebscraperWithUI/src/application/ImageApp.java
@@ -1,62 +1,40 @@
 package application;
-import javafx.animation.KeyFrame;
-import javafx.scene.layout.StackPane;
-import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.Node;
+
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import javafx.scene.layout.VBox;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.layout.Pane;
+
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.control.Separator;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.event.ActionEvent;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.event.*;
-import javafx.scene.layout.TilePane;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.stream.Collectors;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-
-import java.lang.Runnable;
-import java.lang.Thread;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.application.Application;
-import javafx.stage.Stage;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.parser.*;
 import org.jsoup.select.Elements;
 
+/**
+ * Webscrapes Yahoo Finance for information about the searched for stock. 
+ * Takes in the stock token and outputs : Previous Close, Open, Bid, Ask,
+ * Day's Range, 52 Week Range, Volume, Avg.Volume, Market Cap, Beta, PE Ratio,
+ * EPS, Earnings Date, Forward Dividend & Yield, Ex-Dividend Date, and 1y Target
+ * Est. Utilizes Jsoup to gather the data from the website. 
+ * @author aakash
+ * 
+ */
 public class ImageApp extends Application {
 
 	private Button button;
@@ -64,11 +42,16 @@ public class ImageApp extends Application {
 	private TextField textField;
 	private Text title;
 	private TableView<Stocks> table;
-	private ObservableList<Stocks> label1 = FXCollections.observableArrayList();
-	private ObservableList<Stocks> number = FXCollections.observableArrayList();
+	private ObservableList<Stocks> label1 = FXCollections.observableArrayList(); // utilized by TableView 
+	// stores all the different stock elements 
 
 
 	
+	/**
+	 * Runs the UI interface and displays the application to the user. 
+	 * 
+	 * @param stage the window that hosts all of the different UI elements 
+	 */
 	@Override
 	public void start(Stage stage) throws Exception {
 		// TODO Auto-generated method stub
@@ -91,7 +74,7 @@ public class ImageApp extends Application {
 		table.setPlaceholder(new Label("Nothing to display"));
 		table.getColumns().clear();
 		table.getItems().clear();
-		firstCol.setCellValueFactory(new PropertyValueFactory<>("stockName"));
+		firstCol.setCellValueFactory(new PropertyValueFactory<>("stockName")); // all used for table view 
 		secondCol.setCellValueFactory(new PropertyValueFactory<>("stockPrice"));
 		
 		table.setItems(label1);
@@ -113,11 +96,14 @@ public class ImageApp extends Application {
 		
 		EventHandler<ActionEvent> x = value -> {
 			table.getItems().clear();
-			action(textField.getText());
-			table.setItems(label1);
-			firstCol.setText(label1.get(0).getComp());
-			secondCol.setText("Current Price: " + label1.get(0).getCurr());
-		};
+			//action(textField.getText());
+			if (action(textField.getText())) { // if an invalid text is placed in the text bar, nothing happens, otherwise button works as intended
+				table.setItems(label1);
+				firstCol.setText(label1.get(0).getComp());
+				secondCol.setText("Current Price: " + label1.get(0).getCurr());
+			} // if
+			
+		}; // eventHandler
 		
 		button.setOnAction(x);
 		
@@ -134,13 +120,13 @@ public class ImageApp extends Application {
 		stage.sizeToScene();
 		stage.show();
 		
-	}
+	} // start
 	
-	public void action(String text) {
+	public boolean action(String text) {
 		Document website;
 		try {
 			website = Jsoup.connect("https://finance.yahoo.com/quote/" + text.toUpperCase() + "?p=" + text.toUpperCase()).get();
-			Elements prices = website.getElementsByClass("Ta(end) Fw(600) lh(14px)"); //
+			Elements prices = website.getElementsByClass("Ta(end) Fw(600) lh(14px)"); // scrapes the javascript and html elements of the webiste 
 			Elements names = website.getElementsByClass("C($primaryColor) W(51%)");
 			Elements companyName = website.getElementsByClass("D(ib) Fz(18px)");
 			Elements currentPrice = website.getElementsByClass("Trsdu(0.3s) Fw(b) "
@@ -149,15 +135,23 @@ public class ImageApp extends Application {
 			while (counter < prices.size()) {
 				label1.add(new Stocks(names.get(counter).text(),prices.get(counter).text()));
 				counter++;
-			}
+			} // while
 			label1.get(0).setComp(companyName.text());
 			label1.get(0).setCurr(currentPrice.text());
+			return true;
 
 		} catch (Exception e) {
 			System.out.println("Invalid Text Provided");
-		}
-	}
+			return false;
+		} // try 
+	} // action
 	
+	/**
+	 * Class used to store the data of the different elements of the {@code Stock} being examined. Utilized by 
+	 * {@code TableView} and {@code TableColumn}.
+	 * @author aakash
+	 *
+	 */
 	public class Stocks {
 		
 		private final SimpleStringProperty stockName;
@@ -165,47 +159,89 @@ public class ImageApp extends Application {
 		private String stockCompanyName;
 		private String stockCurrentP;
 		
+		/**
+		 * Constructor for class.
+		 * @param name
+		 * @param price
+		 */
 		private Stocks(String name, String price) {
 			this.stockName = new SimpleStringProperty(name);
 			this.stockPrice = new SimpleStringProperty(price);
 			stockCompanyName = "";
 			stockCurrentP = "";
-		}
+		} // Stocks
 		
+		/*
+		 * Provides price of the {@code Stock} when it closed. 
+		 * 
+		 * @return the {@code Stock} price
+		 */
 		public String getStockPrice() {
 			return stockPrice.get();
-		}
+		} // getStockPrice
 		
+		/**
+		 * Provides the name of the {@code Stock}.
+		 * 
+		 * @return the name of the {@code Stock}
+		 */
 		public String getStockName() {
 			return stockName.get();
-		}
+		} // getStockName
 		
+		/**
+		 * Sets the name of the {@code Stock}
+		 * 
+		 * @param name what the stock's name, not token, is 
+		 */
 		public void setStockName(String name) {
 			stockName.set(name.toUpperCase());
-		}
+		} // setStockName
 		
+		/**
+		 * Sets the price of the {@code Stock}
+		 * 
+		 * @param price the price of the {@code Stock} 
+		 */
 		public void setStockPrice(String price) { 
 			stockPrice.set(price);
-		}
+		} // setStockPrice
 		
+		/**
+		 * Returns the company name 
+		 * @return the name of the company 
+		 */
 		public String getComp() {
 			return this.stockCompanyName;
-		}
+		} // getComp 
 		
+		/**
+		 * Returns the current {@code Stock} price that fluctuates
+		 * 
+		 * @return the most current {@code Stock} price, subject to change every refresh 
+		 */
 		public String getCurr() {
 			return this.stockCurrentP;
-		}
+		} // getCurr
 		
+		/**
+		 * Sets the {@code Stock's} company 
+		 * 
+		 * @param x the {@code Stock's} company name 
+		 */
 		public void setComp(String x) {
 			stockCompanyName = x;
-		}
+		} // setComp
 		
+		/**
+		 * Sets the current {@code Stock} price
+		 * 
+		 * @param x the current {@code Stock} price
+		 */
 		public void setCurr(String x) {
 			stockCurrentP = x;
-		}
-	}
-	
-	
+		} // setCurr
+	} // start
 	
 
-}
+} // imageView 
--- WebscraperWithUI/src/application/Main.java
+++ WebscraperWithUI/src/application/Main.java
@@ -5,15 +5,23 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
+/**
+ * Main class to run Javafx. 
+ * @author aakash
+ *
+ */
 public class Main  {
 	
+	/**
+	 * Main method 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		try {
 			Application.launch(ImageApp.class,args);
 		} catch (Exception e) {
 			System.out.println("Error invalid input. Please try again.");
-		}
-	}
-}
+		} // try 
+	} // main
+} // Main 
